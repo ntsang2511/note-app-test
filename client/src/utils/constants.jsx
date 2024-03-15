a89df21a95c7838469a1a1ebbcb16af1a5268d4b
sang <tansang25112003@gmail.com>
2024-03-15 23:05:36 +0700

    update client

--- client/src/utils/constants.jsx
+++ client/src/utils/constants.jsx
@@ -1,2 +1,2 @@
 export const GRAPHQL_SERVER = 'https://note-app-test-server.onrender.com';
-export const GRAPHQL_SUBSCRIPTION_ENPOINT = 'ws://note-app-test-server.onrender.com/graphql'
+export const GRAPHQL_SUBSCRIPTION_ENPOINT = 'wss://note-app-test-server.onrender.com/graphql'
